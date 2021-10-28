cb3d1bf18aeba0b708faf388c11a2440c4ec11d4
Amar Kayed <amar.kayed18@gmail.com>
2021-10-28 11:06:14 +0300

    Added Setters and Getters

--- main.cpp
+++ main.cpp
@@ -9,8 +9,19 @@ class Fractie{
         int a;
         int b;
         int x;
-        
+
+        inline void set_a(int a){this->a = a;}
+        inline void set_b(int b){this->b = b;}
+        inline void set_x(int x){this->x = x;}
+
     public:
+
+        inline int get_a()const{return a;}
+        inline int get_b()const{return b;}
+        inline int get_x()const{return x;}
+
+
+        
 };
 
 
